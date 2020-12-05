ee3c141888f87664bc93a8ce77204152437b4c70
chen jian <3150102408@zju.edu.cn>
2020-12-05 15:25:28 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+}
+
+int main() {
+    int num1, num2;
+    cin>>num1>>num2;
+    vector<int> nums1, nums2;
+    for (int i=0; i<=num1; i++)
+    {
+        int temp;
+        cin>>temp;
+        nums1.push_back(temp);
+    }
+    for (int i=0; i<=num2; i++)
+    {
+        int temp;
+        cin>>temp;
+        nums2.push_back(temp);
+    }
+    cout << "Hello, World!" << endl;
+    return 0;
+}
